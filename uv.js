a31c99d969b2e5410955c4e0f81b105e035963fc
Developer <developer@Creatives-MacBook-Pro.local>
2018-03-18 22:39:03 -0700

    added geolocation errors

--- uv.js
+++ uv.js
@@ -41,12 +41,30 @@ $(document).ready(function() {
     function getLocation() {
 
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getUVIndex);
+            navigator.geolocation.getCurrentPosition(getUVIndex, showError);
         } else {
             $("#vitd-recommendation").text("Geolocation is not supported by this browser.");
         }
     }
 
+    //Geolocation errors
+    function showError(error) {
+        let x = document.getElementById("vitd-recommendation");
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "An unknown error occurred."
+                break;
+        }
+    }
 
 
     //Takes current UV index from API call and returns a UV status phrase
